4be2b301b3eaf9c3fc749c6acd2bfc5450bae1d8
davidg <david.gu@thetrainline.com>
2020-04-09 15:31:01 +0100

    Add init and config

---
+++ init.el
@@ -0,0 +1,61 @@
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(unless (package-installed-p 'spacemacs-theme)
+  (package-refresh-contents)
+  (package-install 'spacemacs-theme))
+
+(global-set-key (kbd "<s-return>") 'ansi-term)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+(when window-system (global-hl-line-mode t))
+(when window-system (global-prettify-symbols-mode t))
+
+;; (require 'rbenv)
+;; (setq rbenv-installation-dir "/usr/local/rbenv")
+;; (add-to-list 'load-path (expand-file-name "/path/to/rbenv.el/"))
+;; (global-rbenv-mode)
+
+(use-package which-key
+  :ensure t
+  :init
+  (which-key-mode))
+
+;; (use-package zerodark-theme
+;;   :ensure t
+;;   :init
+;;     (load-theme 'zerodark t))
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(setq inhibit-startup-message t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (spacemacs-dark)))
+ '(custom-safe-themes
+   (quote
+    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
+ '(package-selected-packages (quote (beacon spacemacs-theme which-key use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "PfEd" :family "xos4 Terminus")))))
